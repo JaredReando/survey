88bdfb96b45ce151759923c9d555fc385ba4e9de
Jared Reando and Robert Bruce <jared@>
2018-10-17 15:55:03 -0700

    Add answer reply form, hidden reply box, and slide toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,15 @@ $(function() {
     var nameLast = $("#nameLast").val();
     var secret = $("#secret").val();
 
+    $(".nameFirst").text(nameFirst);
+    $(".nameLast").text(nameLast);
+    $(".music").text(music);
+    $(".food").text(favFood);
+    $(".favDate").text(favDate);
+    $(".favColor").text(favoriteColor);
+    $(".secret").text(secret);
+
+    $(".story").slideToggle().removeClass("hidden");
     event.preventDefault();
   });
 });
